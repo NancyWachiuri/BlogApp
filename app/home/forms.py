f76b3002f7b5c50021d8fbfb6b715752010288f5
NancyWachiuri <nancywachiuri@gmail.com>
2021-09-28 09:24:59 +0300

    Add pitch form and method to query columns

--- app/home/forms.py
+++ app/home/forms.py
@@ -1,7 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,BooleanField
+from wtforms import StringField,PasswordField,SubmitField,BooleanField, SelectField
 from wtforms.validators import Required,Email,EqualTo, ValidationError
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from ..models import Category
 
 class CategoryForm(FlaskForm):
-    category = StringField('Your Email Address',validators=[Required()])
+    category_name = StringField('Add a new category',validators=[Required()])
     submit = SubmitField('Add Category')
+
+
+class PitchForm(FlaskForm):
+    pitch_title = StringField('Add a pitch title',validators=[Required()])
+    pitch_content = StringField('Add a pitch',validators=[Required()])
+    category_name = QuerySelectField(query_factory= lambda: Category.query.all())
+    submit = SubmitField('Add Pitch')
