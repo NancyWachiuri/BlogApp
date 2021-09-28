643184c715bdc8c9ff5f3b6018646d373ce56143
NancyWachiuri <nancywachiuri@gmail.com>
2021-09-28 09:27:31 +0300

    Add secret key and database url to class config

--- config.py
+++ config.py
@@ -2,9 +2,12 @@ import os
 
 class Config():
  SECRET_KEY = os.environ.get('SECRET_KEY')
+ SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+ SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://nancy:AAaa11..@localhost/pitch'
+    #SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://nancy:AAaa11..@localhost/pitch'
     DEBUG = True
 
 class ProdConfig(Config):
