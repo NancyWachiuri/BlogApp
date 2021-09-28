e8d23d3dbabd1d536a61b0e5ad58265d4f646f2c
NancyWachiuri <nancywachiuri@gmail.com>
2021-09-28 16:57:37 +0300

    Add email config

--- config.py
+++ config.py
@@ -5,6 +5,13 @@ class Config():
  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
  SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+ MAIL_SERVER = 'smtp.googlemail.com'
+ MAIL_PORT = 587
+ MAIL_USE_TLS = True
+ MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+ MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
 
 class DevConfig(Config):
     #SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://nancy:AAaa11..@localhost/pitch'
@@ -17,3 +24,10 @@ config_options = {
 "dev":DevConfig,
 "prod":ProdConfig
 }
+
+
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
