217d73add4b369c74497e265d8efa2e87c112ae6
NancyWachiuri <nancywachiuri@gmail.com>
2021-10-01 18:00:49 +0300

    Add login function

--- app/home/views.py
+++ app/home/views.py
@@ -1,8 +1,8 @@
 from flask import render_template, request, redirect, url_for
 from . import home
 from flask_login import login_required, current_user
-from .forms import CategoryForm, PitchForm, CommentForm
-from ..models import Category, Comment,Pitch, Vote
+from .forms import CategoryForm, BlogForm, CommentForm
+from ..models import Category, Comment,Blog, Vote
 from .. import db
 
 @login_required
@@ -24,39 +24,39 @@ def add_category():
     return render_template("home/add_category.html", category_form = form)
 
 @login_required
-@home.route('/add_pitch', methods=['GET','POST'])
-def add_pitch():
-    form = PitchForm()
+@home.route('/add_blog', methods=['GET','POST'])
+def add_blog():
+    form = BlogForm()
     if form.validate_on_submit():
-        new_pitch = Pitch(
-            pitch_title = form.pitch_title.data, 
-            pitch_content = form.pitch_content.data, 
+        new_blog = Blog(
+            blog_title = form.blog_title.data, 
+            blog_content = form.blog_content.data, 
             category= form.category_name.data.id,
-            pitch_author=current_user.id )
-        db.session.add(new_pitch)
+            blog_author=current_user.id )
+        db.session.add(new_blog)
         db.session.commit()
         return redirect(url_for('home.index'))
-    return render_template("home/pitch.html", pitch_form=form)
+    return render_template("home/blog.html", blog_form=form)
 
-@home.route('/upvote/<int:pitch_id>')
+@home.route('/upvote/<int:blog_id>')
 @login_required
-def upvote(pitch_id):
-    pitch=Pitch.query.filter_by(id=pitch_id).first()
-    print(pitch.pitch_title)
+def upvote(blog_id):
+    blog=Blog.query.filter_by(id=blog_id).first()
+    print(blog.blog_title)
     myvote = 1
-    vote = Vote(upvote=myvote, author = current_user.id, pitch = pitch_id)
+    vote = Vote(upvote=myvote, author = current_user.id, blog = blog_id)
     db.session.add(vote)
     db.session.commit()
     return redirect(url_for("home.index"))
 
 
-@home.route('/downvote/<int:pitch_id>')
+@home.route('/downvote/<int:blog_id>')
 @login_required
-def downvote(pitch_id):
-    pitch=Pitch.query.filter_by(id=pitch_id).first()
-    print(pitch.pitch_title)
+def downvote(blog_id):
+    blog=Blog.query.filter_by(id=blog_id).first()
+    print(blog.blog_title)
     mydownvote = 1
-    vote = Vote(downvote=mydownvote, author = current_user.id, pitch = pitch_id)
+    vote = Vote(downvote=mydownvote, author = current_user.id, blog = blog_id)
     db.session.add(vote)
     db.session.commit()
     return redirect(url_for("home.index"))
@@ -66,12 +66,12 @@ def downvote(pitch_id):
 @login_required
 def new_comment(id):
     form = CommentForm()
-    pitch = Pitch.query.filter_by(id=id).first()
+    blog = Blog.query.filter_by(id=id).first()
     if form.validate_on_submit():
 
         comment = form.comment_content.data
 
-        new_comment = Comment(pitch=pitch.id,comment=comment,user_id=current_user.id)
+        new_comment = Comment(blog=blog.id,comment=comment,user_id=current_user.id)
 
         new_comment.save_comment()
         return redirect(url_for('.index',))
